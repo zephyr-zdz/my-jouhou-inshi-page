21c26de21c2e6e242b063415f75b0d24fef17779
Dazhi Zhou <dazhi527@hotmail.com>
2024-07-30 12:17:52 +0900

    edite: baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-//     baseUrl: "inshi-notes.zephyr-zdz.space",
+    baseUrl: "inshi-notes.zephyr-zdz.space",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
